0f48e15038d2bc8f9ab471d655e6b867d33b8ec6
pranjlahu <126371854+pranjlahu@users.noreply.github.com>
2023-04-21 15:04:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,7 +32,7 @@ async def op(_, m :Message):
     button=InlineKeyboardMarkup([[
        InlineKeyboardButton("Add To Group/Channel", url="t.me/Auto_Request_Approvel_bot?startgroup=true")
      ]])
-    await m.reply_text(text="**🦊 Hello {}!\nI'm an auto approve Admin Join Requests Bot.\nI can approve users in Groups/Channels.\nAdd me to your chat and promote me to admin with add members permission.\n\n".format(m.from_user.mention, "https://t.me/telegram/153"), reply_markup=button)
+    await m.reply_text("https://telegra.ph/file/38f34544ea576f9ab0c1b.jpg", text="**🦊 Hello {}!\nI'm an auto approve Admin Join Requests Bot.\nI can approve users in Groups/Channels.\nAdd me to your chat and promote me to admin with add members permission.\n\n".format(m.from_user.mention, "https://t.me/telegram/153"), reply_markup=button)
     
     #━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ callback ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
