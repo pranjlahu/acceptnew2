32e47c71d353790cfab32657acff6ae9d421f9fb
pranjlahu <126371854+pranjlahu@users.noreply.github.com>
2023-04-21 14:43:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ gif = [
 async def op(_, m :Message):
     add_user(m.from_user.id) 
     button=InlineKeyboardMarkup([[
-       InlineKeyboardButton("Add To Group/Channel", url="http://t.me/{approvedbot.username}?startgroup=botstart")
+       InlineKeyboardButton("Add To Group/Channel", url="t.me/Auto_Request_Approvel_bot?startgroup=true")
      ]])
     await m.reply_text(text="**🦊 Hello {}!\nI'm an auto approve Admin Join Requests Bot.\nI can approve users in Groups/Channels.Add me to your chat and promote me to admin with add members permission.**".format(m.from_user.mention, "https://t.me/telegram/153"), reply_markup=button)
     
@@ -46,7 +46,7 @@ async def chk(_, cb : CallbackQuery):
             keyboard = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("Add To Group/Channel", url="http://t.me/{Auto_Request_Approvel_bot.username}?startgroup=botstart")
+                        InlineKeyboardButton("Add To Group/Channel", url="t.me/Auto_Request_Approvel_bot?startgroup=true")
                     ]
                 ]
             )
@@ -145,10 +145,10 @@ async def approve(_, m : Message):
         add_group(m.chat.id)
         await app.approve_chat_join_request(op.id, kk.id)
         button = [[
-            InlineKeyboardButton("Add To Group/Channel", url="http://t.me/{approvedbot.username}?startgroup=botstart")
+            InlineKeyboardButton("Add To Group/Channel", url="t.me/Auto_Request_Approvel_bot?startgroup=true")
         ]]
         markup = InlineKeyboardMarkup(button)
-        caption=f"**Hello {m.from_user.mention}!\nYour Request To Join {m.chat.title} Was Approved\n\n_Powered By @MR_LINK_Z**"
+        caption=f"**Hello {m.from_user.mention}!\nYour Request To Join {m.chat.title} Was Approved\n\n**"
         img = random.choice(gif)
         await app.send_photo(
             m.from_user.id, 
